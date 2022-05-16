92da161ef12fb6ccc139e99e2d5382713f5a5021
Celso Gabriel Dutra Almeida Malosto <93917898+devgabmal@users.noreply.github.com>
2022-05-16 20:33:33 -0300

    Resolve o exercício 03 da aula 04

--- aula04/main.cpp
+++ aula04/main.cpp
@@ -6,23 +6,30 @@ using namespace std;
 int main()
 {
     int idade;
-    string matricula;
+    string matricula, nome;
 
-    Aluno aluno("Andre", "2022010005");
+    //Aluno aluno("Andre", "2022010005");
     //aluno.leNotas();
 
     //cout << "Media: " << aluno.calculaMedia() << endl;
 
+    // INTERFACE
+    cout << "== CADASTRO DO ALUNO ==" << endl;
+    cout << "Digite o nome: ";
+    cin >> nome;
     cout << "Digite a idade: ";
     cin >> idade;
     cout << "Digite a matricula: ";
     cin >> matricula;
 
+    Aluno aluno(nome, matricula);
     aluno.setIdade(idade);
     aluno.setMatricula(matricula);
 
-    cout << "Idade = " << aluno.getIdade() << endl;
-    cout << "Matricula = " << aluno.getMatricula() << endl;
+    cout << endl << "== REGISTRO DO ALUNO ==" << endl;
+    cout << "Nome: " << aluno.getNome() << endl;
+    cout << "Idade: " << aluno.getIdade() << endl;
+    cout << "Matricula: " << aluno.getMatricula() << endl;
 
     return 0;
 }
