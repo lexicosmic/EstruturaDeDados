6cc9723d8fc88559f8fb75ee54894800742623de
devgabmal <celsogabriel.malosto@estudante.ufjf.br>
2022-07-10 05:31:14 -0300

    Atualiza exercícios sobre pilha encadeada

--- lista_exercicios/lista_av2/pilhas/pilha_encadeada/main.cpp
+++ lista_exercicios/lista_av2/pilhas/pilha_encadeada/main.cpp
@@ -20,6 +20,28 @@ int getTopo(PilhaEncad *pilha)
     }
 }
 
+void exPadrao()
+{
+    PilhaEncad p, p2;
+
+    cout << "Empilhando:" << endl;
+    for (int i = 0; i < TAM; i++)
+    {
+        int val = rand() % 100;
+        p.empilha(val);
+        cout << val << "\t";
+    }
+    cout << endl;
+    cout << "Empilhando:" << endl;
+    for (int i = 0; i < TAM / 2; i++)
+    {
+        int val = rand() % 100;
+        p2.empilha(val);
+        cout << val << "\t";
+    }
+    cout << endl;
+}
+
 void concatenaPilhas(PilhaEncad *p1, PilhaEncad *p2)
 {
     while (!p2->vazia())
@@ -45,7 +67,27 @@ PilhaEncad *copiaPilha(PilhaEncad *p)
     return pNova;
 }
 
-int main()
+PilhaEncad *preenchePilhaCrescente()
+{
+    PilhaEncad *pilha = new PilhaEncad;
+    int n, nTopo;
+    cout << "Preenchendo pilha: " << endl;
+    cin >> n;
+    pilha->empilha(n);
+    nTopo = n;
+    for (int i = 0; i < 299; i++)
+    {
+        cin >> n;
+        if (n > nTopo)
+        {
+            pilha->empilha(n);
+            nTopo = n;
+        }
+    }
+    return pilha;
+}
+
+void ex16()
 {
     PilhaEncad p, p2;
 
@@ -57,6 +99,7 @@ int main()
         cout << val << "\t";
     }
     cout << endl;
+
     cout << "Empilhando:" << endl;
     for (int i = 0; i < TAM / 2; i++)
     {
@@ -101,6 +144,59 @@ int main()
     while (!p2.vazia())
         cout << p2.desempilha() << "\t";
     cout << endl;
+}
+
+void ex18()
+{
+    // Ex18 - 10 minutos e 24 segundos
+    PilhaEncad *p = preenchePilhaCrescente();
+    cout << "Imprimindo:" << endl;
+    while (!p->vazia())
+        cout << p->desempilha() << "\t";
+    cout << endl;
+}
+
+PilhaEncad *conversaoDeDecimal(int decimal, int base)
+{
+    PilhaEncad *pilha = new PilhaEncad;
+    int resto = 0;
+    while (decimal > 0)
+    {
+        resto = decimal % base;
+        decimal = decimal / base;
+        pilha->empilha(resto);
+    }
+    return pilha;
+}
+
+void imprimeNaBase(PilhaEncad *p, int base)
+{
+    while (!p->vazia())
+    {
+        int num = p->desempilha();
+        if (base == 16 && num > 9)
+            cout << (char)(num + 'A' - 10);
+        else
+            cout << num;
+    }
+    cout << endl;
+}
+
+void ex22()
+{
+    // Ex22 - 28 minutos e 36 segundos
+    PilhaEncad *p;
+    int base = 8;
+    p = conversaoDeDecimal(2047, base);
+    imprimeNaBase(p, base);
+}
+
+int main()
+{
+    exPadrao();
+    ex16();
+    ex18();
+    ex22();
 
     return 0;
 }
