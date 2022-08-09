c278b740d8b45d3ec8428c346428ed7edfb55ea2
devgabmal <celsogabriel.malosto@estudante.ufjf.br>
2022-08-09 00:47:35 -0300

    Resolve exercícios da aula 11 de DCC107

--- dcc107/aula11/ArvBinBusca.cpp
+++ dcc107/aula11/ArvBinBusca.cpp
@@ -3,6 +3,20 @@
 
 using namespace std;
 
+void rotinaErro(int erro)
+{
+    switch (erro)
+    {
+    case 1:
+        cout << "Arvore vazia!" << endl;
+        break;
+    default:
+        break;
+    }
+    if (erro < 0)
+        exit(erro * -1);
+}
+
 ArvBinBusca::ArvBinBusca()
 {
     raiz = NULL;
@@ -18,16 +32,16 @@ void ArvBinBusca::insere(int val)
     raiz = auxInsere(raiz, val);
 }
 
-NoArv* ArvBinBusca::auxInsere(NoArv *p, int val)
+NoArv *ArvBinBusca::auxInsere(NoArv *p, int val)
 {
-    if(p == NULL)
+    if (p == NULL)
     {
         p = new NoArv();
         p->setInfo(val);
         p->setEsq(NULL);
         p->setDir(NULL);
     }
-    else if(val < p->getInfo())
+    else if (val < p->getInfo())
         p->setEsq(auxInsere(p->getEsq(), val));
     else
         p->setDir(auxInsere(p->getDir(), val));
@@ -41,57 +55,26 @@ bool ArvBinBusca::busca(int val)
 
 bool ArvBinBusca::auxBusca(NoArv *p, int val)
 {
-    if(p == NULL)
+    if (p == NULL)
         return false;
-    else if(p->getInfo() == val)
+    else if (p->getInfo() == val)
         return true;
-    else if(val < p->getInfo())
+    else if (val < p->getInfo())
         return auxBusca(p->getEsq(), val);
     else
         return auxBusca(p->getDir(), val);
 }
 
-void ArvBinBusca::remove(int val)
-{
-    raiz = auxRemove(raiz, val);
-}
-
-NoArv* ArvBinBusca::auxRemove(NoArv *p, int val)
-{
-    if(p == NULL)
-        return NULL;
-    else if(val < p->getInfo())
-        p->setEsq(auxRemove(p->getEsq(), val));
-    else if(val > p->getInfo())
-        p->setDir(auxRemove(p->getDir(), val));
-    else
-    {
-        if(p->getEsq() == NULL && p->getDir() == NULL)
-            p = removeFolha(p);
-        else if((p->getEsq() == NULL) || (p->getDir() == NULL))
-            p = remove1Filho(p);
-        else
-        {
-            NoArv *aux = menorSubArvDireita(p);
-            int tmp = aux->getInfo();
-            p->setInfo(tmp);
-            aux->setInfo(val);
-            p->setDir(auxRemove(p->getDir(), val));
-        }
-    }
-    return p;
-}
-
-NoArv* ArvBinBusca::removeFolha(NoArv *p)
+NoArv *ArvBinBusca::removeFolha(NoArv *p)
 {
     delete p;
     return NULL;
 }
 
-NoArv* ArvBinBusca::remove1Filho(NoArv *p)
+NoArv *ArvBinBusca::remove1Filho(NoArv *p)
 {
     NoArv *aux;
-    if(p->getEsq() == NULL)
+    if (p->getEsq() == NULL)
         aux = p->getDir();
     else
         aux = p->getEsq();
@@ -99,30 +82,32 @@ NoArv* ArvBinBusca::remove1Filho(NoArv *p)
     return aux;
 }
 
-NoArv* ArvBinBusca::menorSubArvDireita(NoArv *p)
+NoArv *ArvBinBusca::menorSubArvDireita(NoArv *p)
 {
     NoArv *aux = p->getDir();
-    while(aux->getEsq() != NULL)
+    while (aux->getEsq() != NULL)
         aux = aux->getEsq();
     return aux;
 }
 
 void ArvBinBusca::imprime()
 {
-    imprimePorNivel(raiz, 0);
+    if (raiz == NULL)
+        rotinaErro(1);
+    else
+        imprimePorNivel(raiz, 0);
 }
 
 void ArvBinBusca::imprimePorNivel(NoArv *p, int nivel)
 {
-    if(p != NULL)
-    {
-        cout << "(" << nivel << ")";
-        for(int i = 1; i <= nivel; i++)
-            cout << "--";
-        cout << p->getInfo() << endl;
-        imprimePorNivel(p->getEsq(), nivel+1);
-        imprimePorNivel(p->getDir(), nivel+1);
-    }
+    if (p == NULL)
+        return;
+    imprimePorNivel(p->getDir(), nivel + 1);
+    cout << "(" << nivel << ")";
+    for (int i = 0; i < nivel; i++)
+        cout << "\t";
+    cout << p->getInfo() << endl;
+    imprimePorNivel(p->getEsq(), nivel + 1);
 }
 
 ArvBinBusca::~ArvBinBusca()
@@ -130,9 +115,9 @@ ArvBinBusca::~ArvBinBusca()
     raiz = libera(raiz);
 }
 
-NoArv* ArvBinBusca::libera(NoArv *p)
+NoArv *ArvBinBusca::libera(NoArv *p)
 {
-    if(p != NULL)
+    if (p != NULL)
     {
         p->setEsq(libera(p->getEsq()));
         p->setDir(libera(p->getDir()));
@@ -146,16 +131,165 @@ float ArvBinBusca::mediaCaminho(int ch)
 {
     int soma = 0, cont = 0;
     NoArv *p = raiz;
-    while(p != NULL)
+    while (p != NULL)
     {
         cont++;
         soma += p->getInfo();
-        if(ch == p->getInfo())
+        if (ch == p->getInfo())
             break;
-        else if(ch > p->getInfo())
+        else if (ch > p->getInfo())
             p = p->getDir();
         else
             p = p->getEsq();
     }
-    return (float)soma/cont;
+    return (float)soma / cont;
+}
+
+// Ex01
+int ArvBinBusca::auxMaior(NoArv *p)
+{
+    NoArv *dir = p->getDir();
+    if (dir == NULL)
+        return p->getInfo();
+    else
+        return auxMaior(dir);
+}
+int ArvBinBusca::maior()
+{
+    if (raiz == NULL)
+        return 0;
+    else
+        return auxMaior(raiz);
+}
+
+// Ex02
+int ArvBinBusca::auxMenor(NoArv *p)
+{
+    NoArv *esq = p->getEsq();
+    if (esq == NULL)
+        return p->getInfo();
+    else
+        return auxMenor(esq);
+}
+int ArvBinBusca::menor()
+{
+    if (raiz == NULL)
+        return 0;
+    else
+        return auxMenor(raiz);
+}
+
+// Ex03
+NoArv *ArvBinBusca::auxRemoveMaior(NoArv *p)
+{
+    NoArv *dir = p->getDir();
+    if (dir == NULL)
+    {
+        delete p;
+        p = NULL;
+    }
+    else
+        p->setDir(auxRemoveMaior(dir));
+    return p;
+}
+void ArvBinBusca::removeMaior()
+{
+    if (raiz == NULL)
+    {
+        rotinaErro(1);
+        return;
+    }
+    NoArv *esq = raiz->getEsq();
+    NoArv *p = auxRemoveMaior(raiz);
+    if (p == NULL)
+        raiz = esq;
+}
+
+// Ex04
+NoArv *ArvBinBusca::auxRemoveMenor(NoArv *p)
+{
+    NoArv *esq = p->getEsq();
+    if (esq == NULL)
+    {
+        delete p;
+        p = NULL;
+    }
+    else
+        p->setEsq(auxRemoveMenor(esq));
+    return p;
+}
+void ArvBinBusca::removeMenor()
+{
+    if (raiz == NULL)
+    {
+        rotinaErro(1);
+        return;
+    }
+    NoArv *dir = raiz->getDir();
+    NoArv *p = auxRemoveMenor(raiz);
+    if (p == NULL)
+        raiz = dir;
+}
+
+// Ex05
+int ArvBinBusca::contaParesCaminho(int x)
+{
+    int qtdPares = 0;
+    NoArv *p = raiz;
+    int info = 0;
+    while (p != NULL)
+    {
+        info = p->getInfo();
+        if (info % 2 == 0)
+            qtdPares++;
+        if (info == x)
+            break;
+        else if (info > x)
+            p = p->getEsq();
+        else
+            p = p->getDir();
+    }
+    return qtdPares;
+}
+
+// Ex06
+NoArv *ArvBinBusca::maiorSubArvEsquerda(NoArv *p)
+{
+    if (p == NULL)
+        return NULL;
+    NoArv *aux = p->getEsq();
+    while (aux->getDir() != NULL)
+        aux = aux->getDir();
+    return aux;
+}
+
+void ArvBinBusca::remove(int val)
+{
+    raiz = auxRemove(raiz, val);
+}
+
+NoArv *ArvBinBusca::auxRemove(NoArv *p, int val)
+{
+    if (p == NULL)
+        return NULL;
+    else if (p->getInfo() < val)
+        p->setDir(auxRemove(p->getDir(), val));
+    else if (p->getInfo() > val)
+        p->setEsq(auxRemove(p->getEsq(), val));
+    else
+    {
+        if (p->getEsq() == NULL && p->getDir() == NULL)
+            p = removeFolha(p);
+        else if (p->getEsq() == NULL || p->getDir() == NULL)
+            p = remove1Filho(p);
+        else
+        {
+            NoArv *aux = maiorSubArvEsquerda(p);
+            int auxInt = aux->getInfo();
+            p->setInfo(auxInt);
+            aux->setInfo(val);
+            p->setEsq(auxRemove(p->getEsq(), val));
+        }
+    }
+    return p;
 }
--- dcc107/aula11/main.cpp
+++ dcc107/aula11/main.cpp
@@ -3,45 +3,229 @@
 
 using namespace std;
 
+// Ex07
+void preencheABB(ArvBinBusca *arv, int inicio, int fim)
+{
+    int media = (inicio + fim) / 2;
+    if (media != fim)
+    {
+        arv->insere(media);
+        preencheABB(arv, inicio, media);
+        preencheABB(arv, media + 1, fim);
+    }
+}
+
 int main()
 {
-    ArvBinBusca arv;
+    ArvBinBusca arvCompleta;
+    ArvBinBusca arvVazia;
+    ArvBinBusca arv1No;
+    ArvBinBusca arv1Folha;
+    ArvBinBusca arvCheia;
+    ArvBinBusca arvTorta;
 
     cout << "Criando Arvore Binaria de Busca (ABB)" << endl;
-    arv.insere(15);
-    arv.insere(9);
-    arv.insere(20);
-    arv.insere(7);
-    arv.insere(11);
-    arv.insere(18);
-    arv.insere(24);
-    arv.insere(6);
-    arv.insere(8);
-    arv.insere(10);
-    arv.insere(12);
-    arv.insere(17);
-    arv.insere(19);
-    arv.imprime();
+    arvCompleta.insere(15);
+    arvCompleta.insere(9);
+    arvCompleta.insere(20);
+    arvCompleta.insere(7);
+    arvCompleta.insere(11);
+    arvCompleta.insere(18);
+    arvCompleta.insere(24);
+    arvCompleta.insere(6);
+    arvCompleta.insere(8);
+    arvCompleta.insere(10);
+    arvCompleta.insere(12);
+    arvCompleta.insere(17);
+    arvCompleta.insere(19);
+
+    arv1No.insere(14);
+
+    arv1Folha.insere(67);
+    arv1Folha.insere(20);
+
+    arvCheia.insere(15);
+    arvCheia.insere(9);
+    arvCheia.insere(20);
+    arvCheia.insere(7);
+    arvCheia.insere(11);
+    arvCheia.insere(18);
+    arvCheia.insere(24);
+
+    arvTorta.insere(15);
+    arvTorta.insere(9);
+    arvTorta.insere(20);
+    arvTorta.insere(7);
+    arvTorta.insere(11);
+    arvTorta.insere(10);
+    arvTorta.insere(32);
+    arvTorta.insere(25);
+    arvTorta.insere(24);
+    arvTorta.insere(5);
+    arvTorta.insere(8);
+    arvTorta.insere(10);
+    arvTorta.insere(6);
+    arvTorta.insere(4);
+    arvTorta.insere(3);
+    arvTorta.insere(12);
+    arvTorta.insere(17);
+    arvTorta.insere(19);
+
+    // cout << "Media do caminho ate o no com valor 18: " << arvCompleta.mediaCaminho(18) << endl;
+
+    // cout << "Busca o valor 20: " << arvCompleta.busca(20) << endl;
+    // cout << "Busca o valor  5: " << arvCompleta.busca(5) << endl;
+    // cout << endl;
+
+    // cout << "Removendo os valores 24, 6, 7, 12, 18, 5." << endl;
+    // arvCompleta.remove(24);
+    // arvCompleta.remove(6);
+    // arvCompleta.remove(7);
+    // arvCompleta.remove(12);
+    // arvCompleta.remove(18);
+    // arvCompleta.remove(5);
+    // cout << endl;
+
+    // cout << "Arvore Binaria de Busca final:" << endl;
+    // arvCompleta.imprime();
+
+    cout << "Vazia: " << endl;
+    arvVazia.imprime();
+    cout << endl
+         << "1 no: " << endl;
+    arv1No.imprime();
+    cout << endl
+         << "1 folha: " << endl;
+    arv1Folha.imprime();
+    cout << endl
+         << "Completa: " << endl;
+    arvCompleta.imprime();
+    cout << endl
+         << "Cheia: " << endl;
+    arvCheia.imprime();
+    cout << endl
+         << "Torta: " << endl;
+    arvTorta.imprime();
     cout << endl;
 
-    cout << "Media do caminho ate o no com valor 18: " << arv.mediaCaminho(18) << endl;
+    // // Ex01
+    // cout << "MAIOR" << endl;
+    // cout << "Vazia: " << arvVazia.maior() << endl;
+    // cout << "1 no: " << arv1No.maior() << endl;
+    // cout << "1 folha: " << arv1Folha.maior() << endl;
+    // cout << "Completa: " << arvCompleta.maior() << endl;
+    // cout << "Cheia: " << arvCheia.maior() << endl;
+    // cout << "Torta: " << arvTorta.maior() << endl;
+    // cout << endl;
 
-    cout << "Busca o valor 20: " << arv.busca(20) << endl;
-    cout << "Busca o valor  5: " << arv.busca(5) << endl;
+    // // Ex02
+    // cout << "MENOR" << endl;
+    // cout << "Vazia: " << arvVazia.menor() << endl;
+    // cout << "1 no: " << arv1No.menor() << endl;
+    // cout << "1 folha: " << arv1Folha.menor() << endl;
+    // cout << "Completa: " << arvCompleta.menor() << endl;
+    // cout << "Cheia: " << arvCheia.menor() << endl;
+    // cout << "Torta: " << arvTorta.menor() << endl;
+    // cout << endl;
+
+    // // Ex03
+    // cout << "REMOVE MAIOR" << endl;
+    // arvVazia.removeMaior();
+    // arv1No.removeMaior();
+    // arv1Folha.removeMaior();
+    // arvCompleta.removeMaior();
+    // arvCheia.removeMaior();
+    // arvTorta.removeMaior();
+    // cout << "Vazia: " << endl;
+    // arvVazia.imprime();
+    // cout << endl
+    //      << "1 no: " << endl;
+    // arv1No.imprime();
+    // cout << endl
+    //      << "1 folha: " << endl;
+    // arv1Folha.imprime();
+    // cout << endl
+    //      << "Completa: " << endl;
+    // arvCompleta.imprime();
+    // cout << endl
+    //      << "Cheia: " << endl;
+    // arvCheia.imprime();
+    // cout << endl
+    //      << "Torta: " << endl;
+    // arvTorta.imprime();
+    // cout << endl;
+
+    // // Ex04
+    // cout << "REMOVE MENOR" << endl;
+    // arvVazia.removeMenor();
+    // arv1No.removeMenor();
+    // arv1Folha.removeMenor();
+    // arvCompleta.removeMenor();
+    // arvCheia.removeMenor();
+    // arvTorta.removeMenor();
+    // cout << "Vazia: " << endl;
+    // arvVazia.imprime();
+    // cout << endl
+    //      << "1 no: " << endl;
+    // arv1No.imprime();
+    // cout << endl
+    //      << "1 folha: " << endl;
+    // arv1Folha.imprime();
+    // cout << endl
+    //      << "Completa: " << endl;
+    // arvCompleta.imprime();
+    // cout << endl
+    //      << "Cheia: " << endl;
+    // arvCheia.imprime();
+    // cout << endl
+    //      << "Torta: " << endl;
+    // arvTorta.imprime();
+    // cout << endl;
+
+    // // Ex05
+    // cout << "PARES NO CAMINHO" << endl;
+    // cout << "Vazia: " << arvVazia.contaParesCaminho(20) << endl;
+    // cout << "1 no: " << arv1No.contaParesCaminho(20) << endl;
+    // cout << "1 folha: " << arv1Folha.contaParesCaminho(20) << endl;
+    // cout << "Completa: " << arvCompleta.contaParesCaminho(17) << endl;
+    // cout << "Cheia: " << arvCheia.contaParesCaminho(11) << endl;
+    // cout << "Torta: " << arvTorta.contaParesCaminho(3) << endl;
+    // cout << endl;
+
+    // // Ex06
+    // cout << "REMOVE SUBSTITUINDO PELO MAIOR A ESQUERDA" << endl;
+    // arvVazia.remove(0);
+    // arv1No.remove(1);
+    // arv1Folha.remove(67);
+    // arvCompleta.remove(18);
+    // arvCheia.remove(15);
+    // arvTorta.remove(5);
+    // cout << "Vazia: " << endl;
+    // arvVazia.imprime();
+    // cout << endl
+    //      << "1 no: " << endl;
+    // arv1No.imprime();
+    // cout << endl
+    //      << "1 folha: " << endl;
+    // arv1Folha.imprime();
+    // cout << endl
+    //      << "Completa: " << endl;
+    // arvCompleta.imprime();
+    // cout << endl
+    //      << "Cheia: " << endl;
+    // arvCheia.imprime();
+    // cout << endl
+    //      << "Torta: " << endl;
+    // arvTorta.imprime();
+    // cout << endl;
+
+    // Ex07
+    ArvBinBusca arvMontada;
+    preencheABB(&arvMontada, 10, 26);
+    cout << endl
+         << "Montada: " << endl;
+    arvMontada.imprime();
     cout << endl;
-
-    cout << "Removendo os valores 24, 6, 7, 12, 18, 5." << endl;
-    arv.remove(24);
-    arv.remove(6);
-    arv.remove(7);
-    arv.remove(12);
-    arv.remove(18);
-    arv.remove(5);
-    cout << endl;
-
-    cout << "Arvore Binaria de Busca final:" << endl;
-    arv.imprime();
 
     return 0;
 }
-
